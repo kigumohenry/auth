5b5b6da2e5ee77e7c1c1b2f7fc19b860eb0405f4
henry <kigumohenry@gmail.com>
2026-03-09 12:43:24 +0300

    add global error handling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const { ZodError } = require("zod");
 
 const authRouter = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
@@ -15,5 +16,22 @@ app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRoutes);
+
+app.use((err, req, res, next) => {
+  if (err instanceof ZodError) {
+    const formattedErrors = err.errors.map((e) => {
+      msg: e.message;
+    });
+
+    return res
+      .status(400)
+      .json({ msg: "Validation error", errors: formattedErrors });
+  }
+
+  //other errors
+  console.error(err);
+
+  res.status(500).json({ msg: "Internal server error" });
+});
 
 mongoose.connect(process.env.MONGO_URI);
